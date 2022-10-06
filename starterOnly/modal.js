6ce7cd0692c5ca48cbcd92a2ecfc6a462a4a71d5
Winters67 <emmanuel.vbg@gmail.com>
2022-10-06 14:16:04 +0200

    regex nom prenon

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,83 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// les regex
 
+const prenom = document.getElementById("first");
+const nom = document.getElementById("last");
+
+// Prénom
+let valuePrenom, valueNom;
+prenom.addEventListener("input", (e) => {
+  valuePrenom;
+  if (e.target.value.length == 0) {
+    erreur = " prénom manquant";
+    console.log("rien");
+    prenom.parentElement.setAttribute("data-error-visible", "true");
+    prenom.parentElement.setAttribute("data-error", erreur);
+    valuePrenom = null;
+    console.log(valuePrenom);
+  } else if (e.target.value.length < 2 || e.target.value.length > 25) {
+    erreur = "veuillez entrer 2 caractères ou plus dans le champ prénom ";
+    prenom.parentElement.setAttribute("data-error-visible", "true");
+    prenom.parentElement.setAttribute("data-error", erreur);
+    valuePrenom = null;
+  }
+
+  if (e.target.value.match(/^[a-z A-Z]{2,25}$/)) {
+    prenom.parentElement.removeAttribute("data-error-visible", "true");
+    prenom.parentElement.removesetAttribute("data-error", erreur);
+    valuePrenom = e.target.value;
+    console.log("succes");
+    console.log(valuePrenom);
+  }
+
+  if (
+    !e.target.value.match(/^[a-z A-Z]{2,25}$/) &&
+    e.target.value.length > 3 &&
+    e.target.value.length < 25
+  ) {
+    erreur = "les caractères spéciaux ne sont pas autorisés";
+    prenom.parentElement.setAttribute("data-error-visible", "true");
+    prenom.parentElement.setAttribute("data-error", erreur);
+    valuePrenom = null;
+  }
+});
+// nom
+nom.addEventListener("input", (e) => {
+  valuePrenom;
+  if (e.target.value.length == 0) {
+    erreur = " Nom manquant";
+    console.log("rien");
+    nom.parentElement.setAttribute("data-error-visible", "true");
+    nom.parentElement.setAttribute("data-error", erreur);
+    valueNom = null;
+    console.log(valueNom);
+  } else if (e.target.value.length < 2 || e.target.value.length > 25) {
+    erreur = "veuillez entrer 2 caractères ou plus dans le champ nom ";
+    nom.parentElement.setAttribute("data-error-visible", "true");
+    nom.parentElement.setAttribute("data-error", erreur);
+    valueNom = null;
+  }
+
+  if (e.target.value.match(/^[a-z A-Z]{2,25}$/)) {
+    nom.parentElement.removeAttribute("data-error-visible", "true");
+    nom.parentElement.removesetAttribute("data-error", erreur);
+    valuePrenom = e.target.value;
+    console.log("succes");
+    console.log(valueNom);
+  }
+
+  if (
+    !e.target.value.match(/^[a-z A-Z]{2,25}$/) &&
+    e.target.value.length > 3 &&
+    e.target.value.length < 25
+  ) {
+    erreur = "les caractères spéciaux ne sont pas autorisés";
+    nom.parentElement.setAttribute("data-error-visible", "true");
+    nom.parentElement.setAttribute("data-error", erreur);
+    valueNom = null;
+  }
+});
+
+// Email
